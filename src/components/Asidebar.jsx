c435f78548f2a8c3ac0dbf74407a07eeb45c628f
Ravi Yadav <shortanswer12345@gmail.com>
2023-03-06 10:17:58 +0530

    add navbar

--- src/components/Asidebar.jsx
+++ src/components/Asidebar.jsx
@@ -11,12 +11,12 @@ const Asidebar = () => {
 
                 <div className='relative w-3/4 m-auto '>
 
-                    <FcOk className='text-xs absolute left-14 top-9' />
-                    <h1 className='border-b-2 pt-3  text-2xl border-green-400 w-fit m-6'>sahinotes</h1>
+                    <FcOk className='text-xs absolute left-12 top-7' />
+                    <h1 className='border-b-2 pt-3  text-2xl border-green-400 w-fit m-4'>sahinotes</h1>
                 </div>
-                <button className='bg-green-700 rounded-3xl font-semibold p-2 w-3/4 m-auto text-white'><BsPlus className='inline mr-1 mb-1 text-white text-2xl font-extrabold' />New notes</button>
-                <Link to='/' className='p-2 border mt-7 font-semibold w-3/4  m-auto' >Home</Link>
-                <Link to='/savednotes' className='p-2 border mt-2 font-semibold w-3/4 m-auto' >Saved Notes</Link>
+                <button className='bg-green-700 mt-4 rounded-3xl font-semibold p-2 w-3/4 m-auto text-white'><BsPlus className='inline mr-1 mb-1 text-white text-2xl font-extrabold' />New notes</button>
+                <Link to='/' className='p-1  mt-5 font-semibold w-3/4  m-auto' >Home</Link>
+                <Link to='/savednotes' className='p-1 font-semibold w-3/4 m-auto' >Saved Notes</Link>
             </div>
         </>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import {FiSearch} from 'react-icons/fi'
 
 const Navbar = () => {
   return (
 
     <>
-    <div className=' p-4 h-28'>
-        <input type="search" placeholder='Search the notes, teacher..' className='p-2 border w-96 ' />
+    <div className='p-4 '>
+      <div className='mt-5 w-3/4 m-auto '>
+        <input type="search" placeholder='Search the notes, teacher..' className='p-[7px] border w-96 rounded-l-lg inline ' />
+        <span className='rounded-r-lg'>
+        <FiSearch className='p-[10px] mb-1 font-semibold bg-green-700 text-4xl text-white inline'  />
+         </span> 
+      </div>
+      <div className='mt-2 h-36 bg-gradient-to-r from-pink-300 to-blue-300'>
+        
+      </div>
     </div>
     </>
   )
