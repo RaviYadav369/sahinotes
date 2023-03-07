13bca7964a91382cb1291f13457d6d8037a67601
Ravi Yadav <shortanswer12345@gmail.com>
2023-03-07 14:28:50 +0530

    complete home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
-import {FiSearch} from 'react-icons/fi'
+import { FiSearch } from 'react-icons/fi'
+import {TbNumber3 , TbNumber1} from 'react-icons/tb'
 
 const Navbar = () => {
   return (
 
     <>
-    <div className='p-4 '>
-      <div className='mt-5 w-3/4 m-auto '>
-        <input type="search" placeholder='Search the notes, teacher..' className='p-[7px] border w-96 rounded-l-lg inline ' />
-        <span className='rounded-r-lg'>
-        <FiSearch className='p-[10px] mb-1 font-semibold bg-green-700 text-4xl text-white inline'  />
-         </span> 
+      <div className='p-4 '>
+        <div className='mt-5 w-3/4 m-auto '>
+          <input type="search" placeholder='Search the notes, teacher..' className='p-[7px] border w-96 rounded-l-lg inline ' />
+          <span className='rounded-r-lg'>
+            <FiSearch className='p-[10px] mb-1 font-semibold bg-green-700 text-4xl text-white inline' />
+          </span>
+        </div>
+        <div className='mt-2 h-36 bg-gradient-to-r from-pink-300 to-blue-300' />
+        <div className='h-36 mt-0 p-5 bg-gray-100'>
+          <div className='flex justify-between pt-3 pb-3'>
+            <div className='flex'>
+              <div className='h-14 w-14 p-4 font-semibold text-white rounded-full inline bg-green-400' >OK</div>
+              <div className='ml-2 pt-1'>
+                <h2 className='font-semibold'>Om Khandade</h2>
+                <p className='underline text-sm'>248 subscriber</p>
+              </div>
+            </div>
+            <div>
+              <button className='bg-green-400 text-black rounded p-2 pl-5 pr-5 mt-1'>Subscribe</button>
+            </div>
+          </div>
+          <div className='flex justify-around w-3/5'>
+            <div className='p-2 w-3/12 mr-2 border-b-4 font-semibold border-green-600 flex justify-between'>Notes <TbNumber3 className='bg-green-600 p-1 text-lg mt-1 text-white' /></div>
+            <div className='p-2 w-3/12 mr-2  font-semibold flex justify-between'>NotesLists <TbNumber1 className='bg-gray-500 p-1 text-lg text-white mt-1' /></div>
+            <div className='p-2 w-3/12 mr-2  font-semibold '>About</div>
+            
+          </div>
+        </div>
       </div>
-      <div className='mt-2 h-36 bg-gradient-to-r from-pink-300 to-blue-300'>
-        
-      </div>
-    </div>
     </>
   )
 }
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Asidebar from './Asidebar'
 import Navbar from './Navbar'
+import { FcOk } from 'react-icons/fc'
 
 const Notes = () => {
     return (
@@ -12,7 +13,85 @@ const Notes = () => {
                 <div className='col-span-4 row-span-1'>
                     <Navbar />
                 </div>
-                <h2 className='col-span-4 row-span-4'>Notes</h2>
+                <div className='col-span-4 row-span-4 p-3'>
+                    <div className='grid grid-rows-3 grid-cols-3 gap-3'>
+                        <div className='p-3 border relative '>
+                            <div className='absolute h-full w-full  '>
+                                <div className='bg-black w-5 text-sm p-[1px] left-[87px] top-[132px] absolute text-white'>52</div>
+                                <FcOk className='absolute top-28 right-7' />
+                            </div>
+                            <div className='flex justify-between'>
+                                <div className='w-2/5 h-32'>
+                                    <img src='https://images.unsplash.com/photo-1586075574644-b6e4cb9df495?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTZ8fGE0JTIwc2l6ZSUyMHBhcGVyfGVufDB8fDB8fA%3D%3D' alt='note1' className='h-full w-full' />
+                                </div>
+                                <div className='w-4/5 ml-3'>
+                                    <p className='font-semibold text-sm'>First Year: Dental Anatomy - Bones Complete (4 topic)</p>
+                                    <div className='mt-4'>
+                                        <p className='inline text-xs '>51K views </p>
+                                        <p className='inline text-xs'>2 days ago </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='p-3 border relative '>
+                            <div className='absolute h-full w-full  '>
+                                <div className='bg-black w-5 text-sm p-[1px] left-[87px] top-[132px] absolute text-white'>5</div>
+                                <FcOk className='absolute top-28 right-7' />
+                            </div>
+                            <div className='flex justify-between'>
+                                <div className='w-2/5 h-32'>
+                                    <img src='https://images.unsplash.com/photo-1586075574644-b6e4cb9df495?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTZ8fGE0JTIwc2l6ZSUyMHBhcGVyfGVufDB8fDB8fA%3D%3D' alt='note1' className='h-full w-full' />
+                                </div>
+                                <div className='w-4/5 ml-3'>
+                                    <p className='font-semibold text-sm'>CIIPS: Triz benchmarking (Lecture 8) </p>
+                                    <div className='mt-4'>
+                                        <p className='inline text-xs '>29K views </p>
+                                        <p className='inline text-xs'>6 days ago </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='p-3 border relative '>
+                            <div className='absolute h-full w-full  '>
+                                <div className='bg-black w-5 text-sm p-[1px] left-[87px] top-[132px] absolute text-white'>5</div>
+                                <FcOk className='absolute top-28 right-7' />
+                            </div>
+                            <div className='flex justify-between'>
+                                <div className='w-2/5 h-32'>
+                                    <img src='https://images.unsplash.com/photo-1586075574644-b6e4cb9df495?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTZ8fGE0JTIwc2l6ZSUyMHBhcGVyfGVufDB8fDB8fA%3D%3D' alt='note1' className='h-full w-full' />
+                                </div>
+                                <div className='w-4/5 ml-3'>
+                                    <p className='font-semibold text-sm'>JEE: Organic Chemistry - Halogens in 5 minutes</p>
+                                    <div className='mt-4'>
+                                        <p className='inline text-xs '>In Draft</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                     
+                        <div className='p-3 border relative '>
+                            <div className='absolute h-full w-full  '>
+                                <div className='bg-black w-5 text-sm p-[1px] left-[87px] top-[132px] absolute text-white'>5</div>
+                                <FcOk className='absolute top-28 right-7' />
+                            </div>
+                            <div className='flex justify-between'>
+                                <div className='w-2/5 h-32'>
+                                    <img src='https://images.unsplash.com/photo-1586075574644-b6e4cb9df495?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTZ8fGE0JTIwc2l6ZSUyMHBhcGVyfGVufDB8fDB8fA%3D%3D' alt='note1' className='h-full w-full' />
+                                </div>
+                                <div className='w-4/5 ml-3'>
+                                    <p className='font-semibold text-sm'>First Year: Dental Anatomy - Bones Complete (4 topic)</p>
+                                    <div className='mt-4'>
+                                        <p className='inline text-xs '>51K views </p>
+                                        <p className='inline text-xs'>2 days ago </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                     
+
+                    </div>
+
+                </div>
             </div>
         </>
     )
